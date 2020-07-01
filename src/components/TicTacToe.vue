58bcfcf6f1d32ab8f48e6f4c06d518d34f7684f3
Tia Rose <shortyshort789@aol.com>
2020-06-30 21:12:50 -0700

    scaffolded out project, got some functionality working. more work to be done

---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tictactoe-board">
+    <div v-for="(num, i) in 3" v-bind:key=i>
+      <div v-for="(num, j) in 3" v-bind:key=j>
+        <cell @click="performMove(i, j)" :value="board[i][j]"></cell>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+  data() { return {
+    board: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]
+  }},
+
+  methods: {
+    performMove(x, y) {
+      if(this.board[x][y] !== ''){
+        //Not an empty space to move
+        return;
+      }
+      this.board[x][y] = 'x';
+      this.$forceUpdate();
+
+    }
+  }
+
+}
+</script>
+
+<style>
+  .tictactoe-board {
+    display: flex;
+    flex-wrap: wrap;
+    width: 204px;
+    height: 204px;
+  }
+</style>
